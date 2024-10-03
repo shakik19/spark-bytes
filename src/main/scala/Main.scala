e9247c4bf1042859523f75b3f28fab634095972e
shakik19 <1ne9ine.o@gmail.com>
2024-10-03 20:20:54 +0600

    update: solved all the tasks

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,6 +32,7 @@ object Main {
     df.show(10)
     df.printSchema()
 
+
     /**
      *! DOMAIN SPECIFIC LANGUAGE
     */
@@ -50,7 +51,7 @@ object Main {
     // Conclusion, All methods
     df.select(col("Date"), $"Open", df("Close"), highCol, lowCol).show(20)
     val rowCount: Long = df.count()
-    println(rowCount)
+    println(s"Row Count: $rowCount")
     assert(rowCount >= 9909)
 
     /**
@@ -80,21 +81,67 @@ object Main {
 
     //                                                           row[i]   cached as it's repeated next line
     println(s"equalOpenClose : ${df.select(equalOpenClose).filter(_(0) == true).cache().count()}")
-    df.select(col("Open"), col("Close"), equalOpenClose).filter(_.get(2) == true).show(30)
+    df.select(col("Open"), col("Close"), equalOpenClose).filter(_.get(2) == true).show(5)
 
     // Adding a new Column with given column or given expression (matches for every row)
-    df.withColumn("isOpenEqualClose", $"Open" === $"Close").select($"Open", $"Close", $"isOpenEqualClose").show
+    df.withColumn("isOpenEqualClose", $"Open" === $"Close").select($"Open", $"Close", $"isOpenEqualClose").show(5)
+
 
 
     /**
-     *? Task: Find the maximum volume of share for every month in ascending order.
+     *? Task 1: Rename all the column name to 'camel_case'
      */
-    df.createOrReplaceTempView("df")
+    val renamedColumn = Seq(
+      col("Date").as("date"),
+      col("Open").as("open"),
+      col("High").as("high"),
+      col("Low").as("low"),
+      col("Close").as("close"),
+      col("Adj Close").as("adj_close"),
+      col("Volume").as("volume")
+    )
+    df.select(renamedColumn: _*).printSchema()
+    // This approach is the most explicit and simple but requires every name to be strongly specified
+
+    // OR
+    // here df is passed to the lambda func as first arg tempDf and col is the left most element of df.columns seq
+    df.columns.foldLeft(df)((tempDf, col) =>
+      tempDf.withColumnRenamed(col, col.toLowerCase.replaceAll("\\s+", "_"))).printSchema()
+    // This method provides more versatility to control the logic for each column since in each iteration a new df is created
+
+    // OR
+    // This method is more simpler but lacks individual control. Good for uniform renaming use cases
+    df.select(df.columns.map(c => col(c).as(c.toLowerCase.replaceAll("\\s+", "_"))): _*).printSchema()
+    val df2 = df.toDF(df.columns.map(_.toLowerCase.replaceAll("\\s+", "_")): _*)
+    df2.printSchema()
+
+    // the _* indicates to unpack all the elements of the Collection
+
+    // OR using SQL expr.
+
+    /**
+     *? Task 2: Add a column containing the diff between 'open' and 'close'
+     */
+    df2.withColumn("diffOpenClose", df2("open") - df2("close"))
+      .select($"open", $"close", $"diffOpenClose")
+      .show(5)
+
+    /**
+     *? Task 3: Find the days when 'close' price was more that 10% higher than open
+     */
+    df2.select($"date", $"open", $"close")
+      .where($"close" > $"open" * 1.10)
+      .show()
+
+    /**
+     *? Task 4: Find the maximum volume of share for every month in ascending order.
+     */
+    df2.createOrReplaceTempView("df")
     spark.sql(
       """
         |SELECT
-        | DATE_FORMAT(df.Date, "MM-yy") AS month,
-        | MAX(Volume)
+        | DATE_FORMAT(df.date, "MM-yy") AS month,
+        | MAX(volume) AS max_volume
         |FROM
         | df
         |GROUP BY
@@ -102,5 +149,11 @@ object Main {
         |ORDER BY
         | 1
         |""".stripMargin).show()
+    // Equivalent Code
+    df2.withColumn("month", date_format(col("date"), "MMM-yyyy"))
+      .groupBy("month")
+      .agg(max("volume").as("max_volume"))
+      .orderBy($"max_volume".desc_nulls_first)
+      .show()
   }
 }
