aa7a952fb61625e1085e6e2b95720f60633afdd7
shakik19 <1ne9ine.o@gmail.com>
2024-10-06 00:25:58 +0600

    update: refactor

--- src/main/scala/ColumnFunctionality.scala
+++ src/main/scala/ColumnFunctionality.scala
@@ -5,6 +5,7 @@ import org.apache.spark.sql.{Column, SparkSession}
 import org.apache.spark.sql.types._
 
 import scala.::
+
 object ColumnFunctionality {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
@@ -14,32 +15,18 @@ object ColumnFunctionality {
       .getOrCreate()
 
     val company = "AAPL"
-    val applePath = s"dataset/stocks/$company.csv"
-    val stockSchema: StructType = StructType(
-        StructField("Date", DateType) ::
-        StructField("Open", DoubleType) ::
-        StructField("High", DoubleType) ::
-        StructField("Low", DoubleType) ::
-        StructField("Close", DoubleType) ::
-        StructField("Adj Close", DoubleType) ::
-        StructField("Volume", IntegerType) :: Nil
-    )
-    val df = spark.read
-      .option("header", value = true)
-      .schema(stockSchema)
-      .csv(applePath)
-
+    val df = GetDataframe.get(company)
     df.show(10)
     df.printSchema()
 
 
     /**
-     *! DOMAIN SPECIFIC LANGUAGE
-    */
+     * ! DOMAIN SPECIFIC LANGUAGE
+     */
 
     /**
-     *? COLUMN REFERENCES
-    */
+     * ? COLUMN REFERENCES
+     */
     df.select("Date", "Open", "Close").show(10)
     val highCol: Column = df("High")
     val lowCol: Column = col("Low")
@@ -55,9 +42,9 @@ object ColumnFunctionality {
     assert(rowCount >= 9909)
 
     /**
-     *? COLUMN FUNCTIONS
+     * ? COLUMN FUNCTIONS
      */
-    val openPercentage = df("Open").multiply(100).as("Open_Percentage")  // as == alias -> only effects select output
+    val openPercentage = df("Open").multiply(100).as("Open_Percentage") // as == alias -> only effects select output
     // the multiply is eqv. to df("Open") * 100 both symbolic and verbal representations are available
     df.select($"Open", openPercentage).show(10)
     val openPercentage2 = concat(round(openPercentage, 2).cast(StringType), lit("%"))
@@ -85,75 +72,5 @@ object ColumnFunctionality {
 
     // Adding a new Column with given column or given expression (matches for every row)
     df.withColumn("isOpenEqualClose", $"Open" === $"Close").select($"Open", $"Close", $"isOpenEqualClose").show(5)
-
-
-
-    /**
-     *? Task 1: Rename all the column name to 'camel_case'
-     */
-    val renamedColumn = Seq(
-      col("Date").as("date"),
-      col("Open").as("open"),
-      col("High").as("high"),
-      col("Low").as("low"),
-      col("Close").as("close"),
-      col("Adj Close").as("adj_close"),
-      col("Volume").as("volume")
-    )
-    df.select(renamedColumn: _*).printSchema()
-    // This approach is the most explicit and simple but requires every name to be strongly specified
-
-    // OR
-    // here df is passed to the lambda func as first arg tempDf and col is the left most element of df.columns seq
-    df.columns.foldLeft(df)((tempDf, col) =>
-      tempDf.withColumnRenamed(col, col.toLowerCase.replaceAll("\\s+", "_"))).printSchema()
-    // This method provides more versatility to control the logic for each column since in each iteration a new df is created
-
-    // OR
-    // This method is more simpler but lacks individual control. Good for uniform renaming use cases
-    df.select(df.columns.map(c => col(c).as(c.toLowerCase.replaceAll("\\s+", "_"))): _*).printSchema()
-    val df2 = df.toDF(df.columns.map(_.toLowerCase.replaceAll("\\s+", "_")): _*)
-    df2.printSchema()
-
-    // the _* indicates to unpack all the elements of the Collection
-
-    // OR using SQL expr.
-
-    /**
-     *? Task 2: Add a column containing the diff between 'open' and 'close'
-     */
-    df2.withColumn("diffOpenClose", df2("open") - df2("close"))
-      .select($"open", $"close", $"diffOpenClose")
-      .show(5)
-
-    /**
-     *? Task 3: Find the days when 'close' price was more that 10% higher than open
-     */
-    df2.select($"date", $"open", $"close")
-      .where($"close" > $"open" * 1.10)
-      .show()
-
-    /**
-     *? Task 4: Find the maximum volume of share for every month in ascending order.
-     */
-    df2.createOrReplaceTempView("df")
-    spark.sql(
-      """
-        |SELECT
-        | DATE_FORMAT(df.date, "MM-yy") AS month,
-        | MAX(volume) AS max_volume
-        |FROM
-        | df
-        |GROUP BY
-        | 1
-        |ORDER BY
-        | 1
-        |""".stripMargin).show()
-    // Equivalent Code
-    df2.withColumn("month", date_format(col("date"), "MMM-yyyy"))
-      .groupBy("month")
-      .agg(max("volume").as("max_volume"))
-      .orderBy($"max_volume".desc_nulls_first)
-      .show()
   }
 }
